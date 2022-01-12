f78598e41b8eab09f55741cb01c49c09dd10be41
MichaelKaiser1 <michi.kaiser2001@gmail.com>
2022-01-12 18:21:14 +0100

    added buttons to stock detail page

--- lib/pages/stock_detail/stock_detail_page.dart
+++ lib/pages/stock_detail/stock_detail_page.dart
@@ -5,6 +5,53 @@ class StockDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Column(
+        children: [
+          //TODO: Placeholder for Stock
+          Placeholder(
+            fallbackHeight: 150,
+          ),
+          // TODO: Placholder vor Chart
+          Placeholder(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              // TODO: Refactor Buttons
+              ElevatedButton(
+                onPressed: () {},
+                child: Padding(
+                  child: const Text("Kaufen"),
+                  padding: EdgeInsets.all(8),
+                ),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Padding(
+                  child: const Text("Verkaufen"),
+                  padding: EdgeInsets.all(8),
+                ),
+                style: ButtonStyle(
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          // TODO: Placholder Performance and count
+          Placeholder(),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projekt_broker_frontend/pages/stock_detail/stock_detail_page.dart';
 import 'package:projekt_broker_frontend/screens/home/widgets/portfolio_overview_card.dart';
 import 'package:projekt_broker_frontend/services/firebase_auth_service.dart';
 import 'package:projekt_broker_frontend/widgets/stock_overview_card.dart';
@@ -18,6 +19,15 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           children: [
             const Text('Home'),
+            OutlinedButton(
+              onPressed: () => Navigator.of(context)
+                  .push<Widget>(MaterialPageRoute(builder: (context) {
+                return Scaffold(
+                  body: StockDetailPage(),
+                );
+              })),
+              child: Text("Stock Detail"),
+            ),
             OutlinedButton(
               onPressed: context.read<FirebaseAuthService>().signOut,
               child: const Text('Sign out'),
