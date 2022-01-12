9fd1aff1945a5f4fb58473580ca201e86b805951
herophil <philip.herold.01@gmail.com>
2022-01-12 15:04:51 +0100

    fixed testing import

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:broker_frontend/main.dart';
+import 'package:projekt_broker_frontend/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
