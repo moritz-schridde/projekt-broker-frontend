869b96e31c5638289c3bb2763031a2eb1de620f1
NicklasPlatz <57496386+NicklasPlatz@users.noreply.github.com>
2022-05-11 16:13:32 +0200

    added some mock routing

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -2,14 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:flutterfire_ui/auth.dart' as FireFlutter;
 import 'package:projekt_broker_frontend/services/firebase_auth_service.dart';
 
+import '../../widgets/main_bottom_navigation_bar.dart';
+
 class ProfileScreen extends StatelessWidget {
   static const routeName = '/profile';
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FireFlutter.ProfileScreen(
-      providerConfigs: FirebaseAuthService.providerConfigurations,
+    return Scaffold(
+      body: FireFlutter.ProfileScreen(
+        providerConfigs: FirebaseAuthService.providerConfigurations,
+      ),
+      bottomNavigationBar: MainBottomNavigationBar(),
     );
   }
 }
--- lib/screens/stock_search/stock_search_screen.dart
+++ lib/screens/stock_search/stock_search_screen.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
+import '../../widgets/main_bottom_navigation_bar.dart';
+
 class StockSearchScreen extends StatelessWidget {
   static const routeName = "/stock-search";
   const StockSearchScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Center(
+            child: Text("To be developed"),
+          )
+        ],
+      ),
+      bottomNavigationBar: MainBottomNavigationBar(),
+    );
   }
 }
