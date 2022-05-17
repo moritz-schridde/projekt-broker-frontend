8c8a5ac66736ab64d74e53907ea91aa2066fa179
Michael Kaiser <michi.kaiser2001@gmail.com>
2022-05-17 17:31:35 +0200

    designed TextFields

--- lib/screens/buy_stock/buy_stock_screen.dart
+++ lib/screens/buy_stock/buy_stock_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:projekt_broker_frontend/constants/frontend/ui_theme.dart';
 
 class BuyStockScreen extends StatelessWidget {
   static const routeName = "/buy_stock";
@@ -6,45 +8,86 @@ class BuyStockScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
+        //TODO add Aktienname
         title: Text("Aktien kaufen (Aktienname)"),
       ),
       body: Center(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("Gib den gewünschten Kaufbetrag ein:"),
+            const Text("Gib den gewünschten Kaufbetrag ein:"),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Container(
-                  color: Colors.lime,
-                  width: 200,
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      hintText: "7657",
+                //TODO Refector widgets
+                TextField(
+                  keyboardType: TextInputType.number,
+                  style: theme.textTheme.headline3,
+                  cursorColor: UiTheme.textColorBlack,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(
+                      RegExp(r'[0-9]'),
                     ),
+                  ],
+                  decoration: InputDecoration(
+                    //TODO empty text when focus (FocusNode)
+                    hintText: "7657",
+                    suffixIcon: Text(
+                      "€",
+                      style: theme.textTheme.headline3?.copyWith(
+                        color: UiTheme.primaryColor,
+                      ),
+                    ),
+                    constraints: const BoxConstraints(
+                      minWidth: 75,
+                      maxWidth: 150,
+                    ),
+                    suffixIconConstraints: const BoxConstraints(
+                      minWidth: 10,
+                      maxWidth: 20,
+                    ),
+                    border: InputBorder.none,
+                    contentPadding: const EdgeInsets.all(2.0),
                   ),
                 ),
-                Container(
-                  color: Colors.lime,
-                  width: 200,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      hintText: "7657",
+                TextField(
+                  keyboardType: TextInputType.number,
+                  style: theme.textTheme.headline3,
+                  cursorColor: UiTheme.textColorBlack,
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(
+                      RegExp(r'[0-9]'),
                     ),
+                  ],
+                  decoration: InputDecoration(
+                    //TODO empty text when focus (FocusNode)
+                    hintText: "76",
+                    suffixIcon: Text(
+                      "APL",
+                      style: theme.textTheme.headline3?.copyWith(
+                        color: UiTheme.primaryColor,
+                      ),
+                    ),
+                    constraints: const BoxConstraints(
+                      minWidth: 75,
+                      maxWidth: 150,
+                    ),
+                    border: InputBorder.none,
+                    contentPadding: const EdgeInsets.all(2.0),
                   ),
                 ),
               ],
             ),
+            //TODO get Guthaben from backend
             Text("Verfügbares Guthaben: 10000€"),
             Row(
               children: [
                 OutlinedButton(
-                  onPressed: () {},
                   child: Text("10%"),
+                  onPressed: () {},
                 )
               ],
             ),
