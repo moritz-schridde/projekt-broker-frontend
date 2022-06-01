d81a4e84d734da55e6d28d31b24081be5cfcefc3
Michael Kaiser <michi.kaiser2001@gmail.com>
2022-06-01 15:43:29 +0200

    added content to pay in / pay out
    Co-Authored-By: NicklasPlatz <57496386+NicklasPlatz@users.noreply.github.com>

--- lib/screens/home/widgets/portfolio_overview_card.dart
+++ lib/screens/home/widgets/portfolio_overview_card.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:projekt_broker_frontend/constants/frontend/ui_theme.dart';
+import 'package:projekt_broker_frontend/screens/home/widgets/portfolio_overview_button.dart';
 import 'package:projekt_broker_frontend/widgets/draggable_overview.dart';
 
 class PortfolioOverviewCard extends StatelessWidget {
@@ -152,90 +153,7 @@ class PortfolioOverviewCard extends StatelessWidget {
               ),
             ),
           ),
-          Column(
-            children: [
-              Expanded(
-                child: Container(),
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 50),
-                padding: EdgeInsets.symmetric(vertical: 10),
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.5),
-                      spreadRadius: 0,
-                      blurRadius: 2,
-                      offset: const Offset(0, 3), // changes position of shadow
-                    ),
-                  ],
-                  borderRadius: BorderRadius.circular(15.0),
-                  color: Colors.white,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    TextButton(
-                      child: Column(
-                        children: [
-                          Icon(Icons.file_upload_outlined),
-                          Text("Einzahlen"),
-                        ],
-                      ),
-                      // TODO Einzahlen
-                      onPressed: () => showModalBottomSheet(
-                        context: context,
-                        isScrollControlled: true,
-                        barrierColor: Colors.black45,
-                        builder: (context) {
-                          return DraggableOverview(
-                            //TODO change child
-                            child: Container(),
-                            header: "Einzahlen",
-                            inizialSize:
-                                (400 / MediaQuery.of(context).size.height),
-                          );
-                        },
-                      ),
-                    ),
-                    Container(
-                      height: 30,
-                      decoration: BoxDecoration(
-                        border: Border(
-                          left: BorderSide(
-                            color: UiTheme.primaryColorScheme.primary,
-                            width: 2,
-                          ),
-                        ),
-                      ),
-                    ),
-                    TextButton(
-                      child: Column(
-                        children: [
-                          Icon(Icons.file_download_outlined),
-                          Text("Auszahlen"),
-                        ],
-                      ),
-                      onPressed: () => showModalBottomSheet(
-                        context: context,
-                        isScrollControlled: true,
-                        barrierColor: Colors.black45,
-                        builder: (context) {
-                          return DraggableOverview(
-                            //TODO change child
-                            child: Container(),
-                            header: "Auszahlen",
-                            inizialSize:
-                                (400 / MediaQuery.of(context).size.height),
-                          );
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          )
+          PortfolioOverviewButton(),
         ],
       ),
     );
